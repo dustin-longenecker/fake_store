dd8c7271f778563bac2a154f9e4612c18fc418ac
dustin-longenecker <dl-dev@outlook.com>
2021-12-02 14:58:58 -0700

    updated components Header | Product | ProductList / added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Header from './components/Header'
+import ProductList from './components/ProductList'
+import ProductDetail from './components/ProductDetail'
 
 function App() {
   return (
     <div className="App">
-      
+
+    <Router>
+    <Header />
+
+    <Routes>
+    
+      <Route path="/" element={ <ProductList/> } / >
+      <Route path="/product/:productid" element={ <ProductDetail/> } / >
+      <Route> 404 Not Found </Route>
+    </Routes>
+    </Router>      
     </div>
   );
 }
